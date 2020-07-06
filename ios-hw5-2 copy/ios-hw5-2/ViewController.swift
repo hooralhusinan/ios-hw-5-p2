73f45a305c716b90fa8211003ee0b4b4b2a95ea7
hooralhusinan <66676227+hooralhusinan@users.noreply.github.com>
2020-07-07 00:05:57 +0300

    :bee:

---
+++ ios-hw5-2 copy/ios-hw5-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-hw5-2
+//
+//  Created by hoor alhusinan on 7/6/20.
+//  Copyright © 2020 7ooriCodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
